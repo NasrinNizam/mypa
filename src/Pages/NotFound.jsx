371e53fd4bcad435d10eca850db8b5ce74483946
Nasrin Sultana <168544785+NasrinNizam@users.noreply.github.com>
2024-10-03 00:10:43 +0600

    add

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Lottie from "lottie-react";
-import notfound from '../../public/images/NotFound.json';
+import notfound from '../assets/notfound.json';
 export const NotFound = () => {
   return (
     <div className="w-full h-[100vh] bg-gray-400 flex justify-center items-center ">
